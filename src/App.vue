21164dbe9e82e148579e4ac4d02d0f1a87a409f2
Jaime Garza <jimmyg995@gmail.com>
2024-09-15 22:14:55 -0500

    Converting project to a vue framework

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import {ref, onMounted, watch} from 'vue'
+
+document
+    .querySelector("#btn-adding-contact")
+    .addEventListener("click", function() {
+        const nameInput = document.querySelector("#name-input");
+        addContact(nameInput.value);
+        nameInput.value = "";
+    });
+
+function addContact(fullName) {
+    if (!fullName || !fullName.trim()) return;
+
+    const contactDiv = document.createElemnet("div");
+    contactDiv.className = "contact-entry";
+
+    contactDiv.addEventListener("dblclick", function() {
+        contactDiv.remove();
+    });
+    
+    const nameInitialDiv = document.createElement("div");
+    nameInitialDiv.className = "name-initials"
+    nameInitialDiv.textContent = fullName.charArt(0);
+
+    const fullNameDiv = document.createElement("div");
+    fullNameDiv.className = "full-name";
+    fullNameDiv.textContent = fullName;
+
+    contactDiv.append(nameInitialDiv);
+    contactDiv.append(fullNameDiv);
+    document.querySelector("#contact-list").append(contactDiv);
+}
